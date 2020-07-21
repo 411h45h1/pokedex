ae8baca870b6278e3906abf5b4f49bfe8013e7a6
Ahmed <ahmedali.96@hotmail.com>
2020-07-21 18:51:31 -0400

    base api created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [globalPokedexIndex, setGlobalPokedexIndex] = useState(null);
+  const [pokeDexEntry, setPokeDexEntry] = useState();
+
+  useEffect(() => {
+    if (!globalPokedexIndex) {
+      getGlobalPokedexIndex();
+    }
+  }, []);
+
+  const getGlobalPokedexIndex = async () => {
+    let fetchPokedex = await fetch(
+      "https://pokeapi.co/api/v2/pokemon?limit=809"
+    );
+    let pokedexIndex = await fetchPokedex.json();
+    return setGlobalPokedexIndex(pokedexIndex.results);
+  };
+
+  const getEntry = async (entry) => {
+    let pokeDexEntry = globalPokedexIndex.find((obj) => obj.name === entry);
+    let dataURL = pokeDexEntry.url;
+    let entryDataFetch = await fetch(`${dataURL}`);
+    let pokedexDataEntry = await entryDataFetch.json();
+    let primarySprite = pokedexDataEntry.sprites.front_default;
+    let name = pokedexDataEntry.name;
+    let id = pokedexDataEntry.id;
+
+    console.log(`Selected Pokemon: ( #${id}, ${name}) `);
+    return { name, primarySprite, id };
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    globalPokedexIndex && (
+      <div style={{ textAlign: "center" }}>
+        <p
+          onClick={() =>
+            getEntry("snorlax").then((res) => {
+              setPokeDexEntry((prevState) => {
+                return {
+                  ...prevState,
+                  photo: res.primarySprite,
+                  name: res.name,
+                  id: res.id,
+                };
+              });
+            })
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Ahmed's Pokedex
+        </p>
+        {pokeDexEntry ? (
+          <div>
+            <img src={pokeDexEntry.photo} height="250" width="250" />
+            <p>PokeDex Entry: {pokeDexEntry.id}</p>
+
+            <p> Name: {pokeDexEntry.name} </p>
+          </div>
+        ) : null}
+      </div>
+    )
   );
-}
+};
 
 export default App;
