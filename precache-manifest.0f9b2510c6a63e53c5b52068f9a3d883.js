b3f2aea0fa5eb1fa4f431ec4e13edc3c763f1ae2
Ahmed <ahmedali.96@hotmail.com>
2020-07-21 21:34:19 -0400

    Updates

---
+++ precache-manifest.0f9b2510c6a63e53c5b52068f9a3d883.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d44c7fe0621f6193801be44b2a179670",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "b8de87d1597d44a9d2df",
+    "url": "/pokedex/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f8b1407a911a2c85b971",
+    "url": "/pokedex/static/js/2.60e467ee.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/pokedex/static/js/2.60e467ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8de87d1597d44a9d2df",
+    "url": "/pokedex/static/js/main.04c07e47.chunk.js"
+  },
+  {
+    "revision": "ed5a9742913badf7a85c",
+    "url": "/pokedex/static/js/runtime-main.82622ded.js"
+  }
+]);
