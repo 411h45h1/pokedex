dfda50e4d648659cfbbe390205e654b0703bec23
Ahmed <ahmedali.96@hotmail.com>
2020-07-23 19:28:05 -0400

    updated type search output

--- src/components/tools/TypeSearchOutput.js
+++ src/components/tools/TypeSearchOutput.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Grid, Header, Segment } from "semantic-ui-react";
+import { Grid, Label, List, Segment, Image } from "semantic-ui-react";
 import AppContext from "../../context/AppContext";
 import { capitalizeString } from "../";
 
@@ -10,16 +10,37 @@ const TypeSearchOutput = () => {
     <Grid>
       <Grid.Row centered columns="1">
         <Grid.Column>
-          <Segment inverted style={{ minHeight: 500 }}>
+          <Segment inverted style={{ overflow: "auto", maxHeight: 400 }}>
             {firstTypeSelected && (
-              <Segment clearing>
-                <Header as="h3" textAlign="center">
-                  {capitalizeString(firstTypeSelected)}
-                  {secondTypeSelected && " - "}
-                  {secondTypeSelected && capitalizeString(secondTypeSelected)}
-                </Header>
-              </Segment>
+              <div style={{ marginBottom: "5vh" }}>
+                <Label
+                  as="a"
+                  color="blue"
+                  size="huge"
+                  attached="top left"
+                  content={
+                    `${capitalizeString(firstTypeSelected)}` +
+                    `${
+                      secondTypeSelected
+                        ? " - " + capitalizeString(secondTypeSelected)
+                        : ""
+                    }`
+                  }
+                  // onClick={() => handleRandomPokemon()}
+                />
+              </div>
             )}
+
+            <List divided inverted relaxed>
+              <List.Item>
+                <List.Content>
+                  <List.Header as="a">Snickerdoodle</List.Header>
+                  <List.Description>
+                    <Image src={require("../../typeIcons/water.png")} />
+                  </List.Description>
+                </List.Content>
+              </List.Item>
+            </List>
           </Segment>
         </Grid.Column>
       </Grid.Row>
