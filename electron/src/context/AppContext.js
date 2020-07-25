f87da33f9418c38401aaa41378fa2bc5cfb1bffe
Ahmed <ahmedali.96@hotmail.com>
2020-07-24 23:27:37 -0400

    removed bloat

--- electron/src/context/AppContext.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from "../../node_modules/@types/react/node_modules/@types/react";
-
-const AppContext = createContext({
-  globalPokedexIndex: null,
-  pokeDexEntry: null,
-});
-
-export default AppContext;
